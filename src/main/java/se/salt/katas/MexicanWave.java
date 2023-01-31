e99b33f9224b16f7d899e6fe0b38fab07b858ca6
Lukasz Krawiec <lukasz.krawiec@appliedtechnology.se>
2023-01-31 21:55:33 +0100

    Starting MexicanWave kata

---
+++ src/main/java/se/salt/katas/MexicanWave.java
@@ -0,0 +1,20 @@
+package se.salt.katas;
+
+/**
+ * Task
+ * In this simple Kata your task is to create a function that turns a string into a Mexican Wave.
+ * You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up.
+ * <p>
+ * Rules
+ *  1.  The input string will always be lower case but maybe empty.
+ * <p>
+ *  2.  If the character in the string is whitespace then pass over it as if it was an empty seat
+ * <p>
+ * Example
+ * wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+ * <p>
+ * <a href="https://www.codewars.com/kata/58f5c63f1e26ecda7e000029/java">CodeWars Link</a>
+ */
+
+public class MexicanWave {
+}
---
+++ src/test/java/se/salt/katas/MexicanWaveTest.java
@@ -0,0 +1,7 @@
+package se.salt.katas;
+
+import junit.framework.TestCase;
+
+public class MexicanWaveTest extends TestCase {
+
+}
