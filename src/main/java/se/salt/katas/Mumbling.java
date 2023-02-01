b0a01c5e52f6aa53533f787ab823baf4d5d6ed35
Lukasz Krawiec <lukasz.krawiec@appliedtechnology.se>
2023-02-01 22:07:00 +0100

    Working mumble, added tests

--- src/main/java/se/salt/katas/Mumbling.java
+++ src/main/java/se/salt/katas/Mumbling.java
@@ -1,5 +1,7 @@
 package se.salt.katas;
 
+import java.util.stream.IntStream;
+
 /**
  * This time no story, no theory. The examples below show you how to write function accum:
  * <p>
@@ -12,4 +14,19 @@ package se.salt.katas;
  * <a href="https://www.codewars.com/kata/5667e8f4e3f572a8f2000039">CodeWars Link</a>
  */
 public class Mumbling {
+    public String mumble(String mumble) {
+        if (mumble == null) {
+            throw new IllegalArgumentException("Mumble has to be provided");
+        }
+        StringBuilder builder = new StringBuilder();
+        IntStream.range(0, mumble.length())
+                .forEach(it -> {
+                    builder.append(mumble.substring(it, it + 1).toUpperCase());
+                    builder.append(mumble.substring(it, it + 1).toLowerCase().repeat(it));
+                    if (it != mumble.length() - 1) {
+                        builder.append("-");
+                    }
+                });
+        return builder.toString();
+    }
 }
--- src/test/java/se/salt/katas/MumblingTest.java
+++ src/test/java/se/salt/katas/MumblingTest.java
@@ -1,7 +1,39 @@
 package se.salt.katas;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MumblingTest {
 
+    Mumbling mumbling = new Mumbling();
+
+    @Test
+    void shouldRun() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void shouldReturnFirstLetterAsCapital() {
+        assertEquals("A", mumbling.mumble("a"));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "A, a",
+            "A-Bb, ab",
+            "A-Bb-Ccc, abc",
+            "A-Bb-Ccc-Dddd, abcd",
+            "G-Hh-Eee-Oooo-Lllll, gheol",
+            "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy, RqaEzty",
+            "C-Ww-Aaa-Tttt, cwAt",
+            "'',''",
+            "$-##----, $#-"
+
+    })
+    void shouldReturnMumble(String expected, String toTest) {
+        assertEquals(expected, mumbling.mumble(toTest));
+    }
 }
